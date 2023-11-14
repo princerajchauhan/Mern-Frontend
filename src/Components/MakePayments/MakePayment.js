d675adad919e4415ed2d11646827b6267471ea1e
Prince <princeraj.chauhan@outlook.com>
2023-11-14 22:42:56 +0530

    pay

--- src/Components/MakePayments/MakePayment.js
+++ src/Components/MakePayments/MakePayment.js
@@ -1,7 +1,6 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 export const MakePayment = async (prod) => {
-
     const stripe = await loadStripe("pk_test_51O2pl0SGM01vn0zuhMB8YaDsKBiV9XDJdbkITDDoOcMtOzO2LahZILM1gTDmKRpa7yYi14RuYBNealmsubNfp3aW004UlMoyaG")
     const body = {
         product: [{...prod, quantity: 1}]
@@ -11,7 +10,7 @@ export const MakePayment = async (prod) => {
         "Content-Type": "application/json"
     }
 
-    const response = await fetch("http://localhost:8080/pay/create-checkout-session", {
+    const response = await fetch("https://mern-backend-pntb.onrender.com/pay/create-checkout-session", {
         method: "POST",
         headers: headers,
         body: JSON.stringify(body)
